6acfc988175009efed4e768c2d612f4890e634b1
LexieC <dkt4kr@virginia.edu>
2025-07-22 16:09:50 -0400

    updated project page

--- src/projects.jsx
+++ src/projects.jsx
@@ -2,26 +2,39 @@ import React from 'react'
 import wecheck from "./assets/wecheck.png"
 import still from "./assets/yoga.jpg"
 import da from "./assets/da.png"; 
-
+import credit from "./assets/credit.jpg"
+import cloud from "./assets/cloud.png"
 
 const projects = [
   {
     title: 'WeCheck',
-    description: 'A web app to manage roommate bills & chores.',
+    description: 'This is a collaborative web application designed to simplify roommate living by managing shared bills, tracking chores, and promoting accountability through an intuitive interface and real-time updates.',
     link: 'https://wecheck-app.vercel.app',
     image: wecheck,
   },
   {
     title: 'Still',
-    description: 'An AI integrated mood identifier where people can express and learn about their feelings ',
+    description: 'This is a full-stack AI-powered web application that helps users identify, express, and better understand their emotions through mood logging and intelligent feedback. Integrates NLP-based analysis to promote emotional awareness and personal well-being.',
     link: 'https://still-mood.vercel.app/',
     image: still, 
   },
   {
     title: 'Data Analysis for WTV Consulting',
-    description: 'Created interactive dashboards to visualize patient trends and referrals for a health clinic.',
+    description: 'Developed interactive dashboards with a team of fourto analyze patient volume, referral patterns, and clinical trends, enabling data-driven decision-making for healthcare operations.',
     link: 'https://public.tableau.com/app/profile/paige.lynch/viz/WTVDashboardHealthcareOperationalDataInitiative/WTVConsultingDashboard1',
     image: da, 
+  },
+  {
+    title: 'Credit Score Simulator',
+    description: 'This web app Helps users simulate how different financial behaviors affect their credit score.',
+    link: 'https://wecheck-app.vercel.app',
+    image: credit,
+  },
+  {
+    title: 'HybridCloudHub',
+    description: 'This web app simulates a real-world enterprise hybrid IT environment by integrating an on-premises Active Directory with AWS Managed Microsoft AD. It enables federated identity, secure file sharing, and remote access via Amazon WorkSpaces',
+    link: 'https://wecheck-app.vercel.app',
+    image: cloud,
   },
 ];
 
